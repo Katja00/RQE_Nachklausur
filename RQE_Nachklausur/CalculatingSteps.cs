7704cf7ceb1a423933d71309955753e3a80044a8
Christian Lagelstorfer <wi19b076@technikum-wien.at>
2020-12-04 14:11:06 +0100

    Projektdateien hinzufügen.

---
+++ RQE_Nachklausur/CalculatingSteps.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace RQE_Nachklausur
+{
+    [Binding]
+    public class CalculatingSteps
+    {
+        private double x, y, result;
+
+        [Given(@"the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int p0)
+        {
+            x = 10;
+        }
+        
+        [Given(@"the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int p0)
+        {
+            y = 5;
+        }
+
+        [When(@"the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            result = x + y;
+        }
+        
+        [When(@"the two numbers are subtracted")]
+        public void WhenTheTwoNumbersAreSubtracted()
+        {
+            result = x - y;
+        }
+
+        [When(@"the two numbers are divided")]
+        public void WhenTheTwoNumbersAreDivided()
+        {
+            result = x / y;
+        }
+
+        [When(@"the two numbers are multiplied")]
+        public void WhenTheTwoNumbersAreMultiplied()
+        {
+            result = x * y;
+        }
+
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int p0)
+        {
+            var expected = p0;
+            var actual = result;
+            Assert.Equal(expected, actual, 5);
+        }
+    }
+}
---
+++ RQE_Nachklausur/SpecFlowFeature1.feature
@@ -0,0 +1,28 @@
+﻿Feature: Calculating
+	In order to be able to make basic calculation
+	As a arithmetic beginner
+	I want to be told the result of additions, subtractions, divisions and multiplications
+
+Scenario: Add two numbers
+	Given the first number is 10
+	And the second number is 5
+	When the two numbers are added
+	Then the result should be 15
+
+Scenario: Subtract two numbers
+	Given the first number is 10
+	And the second number is 5
+	When the two numbers are subtracted
+	Then the result should be 5
+
+Scenario: Divide two numbers
+	Given the first number is 10
+	And the second number is 5
+	When the two numbers are divided
+	Then the result should be 2
+	
+Scenario: Multiply two numbers
+	Given the first number is 10
+	And the second number is 5
+	When the two numbers are multiplied
+	Then the result should be 50
